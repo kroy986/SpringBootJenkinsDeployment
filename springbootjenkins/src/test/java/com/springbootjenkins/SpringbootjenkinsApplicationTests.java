be3e1bd15afeb374923cbcd409fdc5a3482dae54
Koushik <kroy700133@gmail.com>
2022-12-17 19:16:24 +0530

    Spring boot Jenkins Deployment Demo Changes

---
+++ springbootjenkins/src/test/java/com/springbootjenkins/SpringbootjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
